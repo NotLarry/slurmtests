8293d563c7cea8241c518b6527fbdf30c9ed8b96
notlarry <notlarry@hotmail.com>
2018-12-07 21:19:20 +0000

    using bytearray

--- randomprime.py
+++ randomprime.py
@@ -1,34 +1,48 @@
 import random
 import os
 import errno
+"""
+randomprime.py
 
-# create dictionary primelist
-primelist={}
+This small code is intended to be a test for slurm [https://slurm.schedmd.com/] running on a cluster hat [https://clusterhat.com/].  
+
+Nodes on the clusterhat are limited to a single core and in the current configuration 200mb of RAM.
+
+This program will pick a (random?) number between 1 and 1_000_000, check if it is prime, save that information and move on to the next (random?) number.  The idea is to do this 1_000_000 times.  
+
+The final result is saved to a file.
+
+To save ram and just get a list of what is or isn't prime (with no ordering) I will create a bytearray  
+"""
+# create bytearray primelist
+primelist=bytearray(1000000)
 # set the output file
 outputfile = "/home/notlarry/randomprime/primesearch.results"
 # create the direcotry if it does not exist
 os.makedirs(os.path.dirname(outputfile), exist_ok=True)
 with open(outputfile, "a") as f:
 # for 1000000 itinerations
-    for x in range(0, 1000000):
+    for x in range(0, 100000):
+        # print statement for debugging
+        print(str(x) + " x")
 # generate a random integer between 1 and 100000
         num = random.randint(1,1000000)
+        # print statement for debugging
+        print(str(num) + " num")
     # prime numbers are greater than 1
         if num > 1:
        # check for factors
            for i in range(2,num):
                if (num % i) == 0:
-                   # add to dictionary primelist
-                   primelist["is a prime number."] = (num)
+                   primelist[(num)] = 3 
                    break
            else:
-#               f.write(str (num) + "is a prime number\n")
-                   primelist["is not a prime number."] = (num)
+                   primelist[(num)] = 0
            
     # if input number is less than
     # or equal to 1, it is not prime
         else:
-#           f.write(str (num) + "is not a prime number\n")
-                   primelist["A prime number is not "] = (num)
-        for key,val in primelist.items():
-            f.write(str (val) + (key) + "\n")
+                   primelist[(num)] = 2
+    for val in primelist:
+#            f.write(str (val) + (key) + "\n")
+        print(val)
